f88e0460755f7479c945d0ba2bb7b2425595249a
MrRainz <yue_sum_96@hotmail.com>
2021-02-04 10:29:39 +0800

    fixed some errors

--- nextq/App.js
+++ nextq/App.js
@@ -2,10 +2,10 @@ import React, {useState} from 'react';
 import AsyncStorage from '@react-native-community/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 
-import BottomNavigator from './src/Navigator/BottomNavigator.js'
+import { BottomNavigator } from './src/Navigator/BottomNavigator.js'
 
 export default function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
+  //const [loggedIn, setLoggedIn] = useState(false)
 
   return (
     <NavigationContainer>
